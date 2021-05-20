55f450c8c043dea3498db46f06b927932c820a44
TriusMalarky <51863464+TriusMalarky@users.noreply.github.com>
2021-05-20 12:22:27 -0600

    Currency Reconciliation
    Currency Reconciliation is fully implemented.

--- Warcaster/bin/cmd/reconcile_currency.py
+++ Warcaster/bin/cmd/reconcile_currency.py
@@ -10,6 +10,7 @@ def reconcile_currency(saved):
             if x==str(i+"posrate")or x==str(i+"negrate"):
                 entrypoplist.append(x)
             if x[:-7] in currencies:
+                # Compare pos-neg rates and confirm value if they are not congruent
                 if 'negrate' in x and str(i+'posrate') in currencies[x[:-7]]:
                     if currencies[x[:-7]][str(i+'posrate')] == currencies[i][x]:
                         pass
@@ -18,8 +19,23 @@ def reconcile_currency(saved):
                         newval=int(input(" { What value should they have?\n| "))
                         currencies[x[:-7]][str(i+'posrate')]=newval
                         currencies[i][x]=newval
+                if 'posrate' in x and str(i+'negrate') in currencies[x[:-7]]:
+                    if currencies[x[:-7]][str(i+'negrate')] == currencies[i][x]:
+                        pass
+                    else:
+                        print(" { Values " +str(currencies[x[:-7]]['name'])+" ("+i+"negrate) and "+i+" ("+x+") do not match.")
+                        newval=int(input(" { What value should they have?\n| "))
+                        currencies[x[:-7]][str(i+'negrate')]=newval
+                        currencies[i][x]=newval
+        for k in currencies:
+            if str(k+'negrate') not in currencies[i] and str(k+'posrate') not in currencies[i] and k not in i:
+                inp = input(" { Are "+k+" worth more(type 'M') or less(type 'L') than "+i+"\n| )
+                if inp.upper() == "M":
+                    currencies[i][k+"posrate"]=int(input(" { How many "+i+" is a "+k+" worth?\n| "))
+                elif inp.upper() == "L":
+                    currencies[i][k+"negrate"]=int(input(" { How many "+k+" is a "+i+" worth?\n| "))
                     
         for n in entrypoplist:
             currencies[i].pop(n)
-                
-    print(currencies)
+        newfile=open("Currency/"+str(i)+".txt","w");newfile.write(str(currencies[i]));newfile.close()
+    print(" - Currencies Reconciled - ")
